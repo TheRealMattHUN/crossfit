ec6a069708037cdc1f6015d4e74b9e3d2ab502c2
legolegend007@gmail.com <legolegend007@gmail.com>
2024-06-08 21:37:21 +0200

    ooops

--- src/pages/routes/layout.tsx
+++ src/pages/routes/layout.tsx
@@ -1,19 +1,22 @@
 import {Group, Box, Image} from "@mantine/core"
 import Logo from "../../assets/38464.png"
 import {Link, Outlet} from "react-router-dom";
+import {Suspense} from "react";
 
 function Layout() {
   return (
-    <Box className="root">
-      <Group className="navbar" align="center" gap="xl" justify="center" wrap="nowrap">
-        <Link to="/crossfit/">Kezdőlap</Link>
-        <Link to="/crossfit/about">Rólunk</Link>
-        <Image src={Logo} h={80} w="auto"></Image>
-        <Link to="/crossfit/prices">Áraink</Link>
-        <Link to="/crossfit/contact">Elérhetőségeink</Link>
-      </Group>
-      <Outlet></Outlet>
-    </Box>
+    <Suspense fallback="loading!!">
+      <Box className="root">
+        <Group className="navbar" align="center" gap="xl" justify="center" wrap="nowrap">
+          <Link to="/crossfit/">Kezdőlap</Link>
+          <Link to="/crossfit/about">Rólunk</Link>
+          <Image src={Logo} h={80} w="auto"></Image>
+          <Link to="/crossfit/prices">Áraink</Link>
+          <Link to="/crossfit/contact">Elérhetőségeink</Link>
+        </Group>
+        <Outlet></Outlet>
+      </Box>
+    </Suspense>
   );
 }
 
