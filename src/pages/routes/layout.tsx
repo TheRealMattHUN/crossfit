43a355f8461345733b0d059b8ce8657d1ba52cad
legolegend007@gmail.com <legolegend007@gmail.com>
2024-06-09 09:21:57 +0200

    text

--- src/pages/routes/layout.tsx
+++ src/pages/routes/layout.tsx
@@ -1,4 +1,4 @@
-import {Group, Box, Image} from "@mantine/core"
+import {Group, Box, Image, Text} from "@mantine/core"
 import Logo from "../../assets/38464.png"
 import {Link, Outlet} from "react-router-dom";
 import {Suspense} from "react";
@@ -8,11 +8,11 @@ function Layout() {
     <Suspense fallback="loading!!">
       <Box className="root">
         <Group className="navbar" align="center" gap="xl" justify="center" wrap="nowrap">
-          <Link to="/crossfit/">Kezdőlap</Link>
-          <Link to="/crossfit/about">Rólunk</Link>
+          <Text component={Link} to="/crossfit/">Kezdőlap</Text>
+          <Text component={Link} to="/crossfit/about">Rólunk</Text>
           <Image src={Logo} h={80} w="auto"></Image>
-          <Link to="/crossfit/prices">Áraink</Link>
-          <Link to="/crossfit/contact">Elérhetőségeink</Link>
+          <Text component={Link} to="/crossfit/prices">Áraink</Text>
+          <Text component={Link} to="/crossfit/contact">Elérhetőségeink</Text>
         </Group>
         <Outlet></Outlet>
       </Box>
